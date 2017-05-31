c7cb28d59b45672c9f9b1cb685b9067685a1bac2
Matt Larson <matt.a.larson@gmail.com>
2017-05-31 14:40:29 -0500

    created larger inputs

---
+++ server/models/careprofile.model.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// Mongoose Schema
+var CareProfileSchema = new Schema({
+    imageUrl: {type: String, required: true},
+    name: {type: String, required: true},
+    basicInfo: {type: String, required: true},
+    careInfo: {type: String, required: true}
+});
+
+module.exports = mongoose.model('CareProfile', CareProfileSchema);
