9943abd57a55f3d8758078504f93a719dab6678c
Matt Larson <matt.a.larson@gmail.com>
2017-06-11 15:34:35 -0500

    working on scroll up

--- server/public/assets/scripts/controllers/profile.controller.js
+++ server/public/assets/scripts/controllers/profile.controller.js
@@ -117,7 +117,7 @@ myApp.controller('profileController', ['$scope', '$http', '$location', '$anchorS
   //function to share the profile with another user
   vm.shareProfile = function() {
     console.log('share button clicked!');
-    $location.hash('top');
+    $location.hash('.jumbotron');
     $anchorScroll();
     $location.path('/careshare');
   }; //end shareProfile
